af637c379f787f1f8da22db7d9ae1fd00d1c2061
Karri390 <rik.goku@gmail.com>
2018-09-13 11:14:45 +1200

    Add files via upload

---
+++ final.js
@@ -0,0 +1,14 @@
+// Saptarshi Karmakar 5264263 COMP112 ASSIGNMENT JavaScript
+// skarmakar final.js
+
+
+window.addEventListener("load",imgFunc,false);
+function imgFunc(){
+var bigImage = document.getElementById("bigImage");
+var thumbnailsHolder = document.getElementById("thumbnailsHolder");
+thumbnailsHolder.addEventListener("click",function(event){
+if (event.target.tagName == "IMG"){
+bigImage.src = event.target.src;
+}
+},false);
+}
